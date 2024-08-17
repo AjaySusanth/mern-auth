2edfabb29e8002612dba30a243f755c18317f483
Ajay Susanth <ajaysusanth10@gmail.com>
2024-08-17 11:03:49 +0530

    login-endpoint

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -84,7 +84,50 @@ export const verifyEmail = async (req,res) =>{
 }
 
 export const login = async(req,res)=>{
-    res.send("login route")
+    const {email,password} = req.body
+    try{
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found, Please sign in"
+            })
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password)
+
+        if(!isPasswordValid){
+            return res.status(400).json({
+                success:false,
+                message:"Incorrect password"
+            })
+        }
+
+        generateTokenAndSetCookie(res,user._id)
+        user.lastLogin = new Date()
+
+        await user.save()
+
+        res.status(200).json({
+            success:true,
+            message:"Logged in successfully",
+            user:{
+                ...user._doc,
+                password:undefined
+            }
+        })
+    }
+    catch(error){
+        console.log("Error in login",error)
+        res.status(500).json({
+            success:false,
+            message:`Login failed: ${error.message}`
+        })
+    }
+
+
 }
 export const logout = async(req,res)=>{
     res.clearCookie("token")
